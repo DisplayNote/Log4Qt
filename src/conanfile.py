830e570ee592262bd4d6ce1ae7e167d84e8ee1eb
Alvaro Ruy Perez <apgriver@gmail.com>
2021-08-30 17:01:35 +0200

    Update conanfile

--- src/conanfile.py
+++ src/conanfile.py
@@ -22,14 +22,14 @@ class Log4QtConan(ConanFile):
     generators = 'qmake'
 
     def package(self):
-        self.copy('*', 'log4qt/install/include', 'log4qt/install/include')
-        self.copy("log4qt.dll", src='log4qt/install/lib/bin', dst='log4qt/install/lib', keep_path=False)
-        self.copy("log4qt.lib", src='log4qt/install/lib/bin', dst='log4qt/install/lib', keep_path=False)
+        self.copy("log4qt.dll", src='log4qt/install/lib/bin', dst='log4qt/lib', keep_path=False)
+        self.copy("log4qt.lib", src='log4qt/install/lib/bin', dst='log4qt/lib', keep_path=False)
+        self.copy('*', src='log4qt/install/include', dst='log4qt/include', keep_path=False)
     
     def package_info(self):
         ## Add libraries stored in lib folder
-        self.cpp_info.libdirs = ["log4qt/install/lib"]
-        self.cpp_info.libs = tools.collect_libs(self, 'log4qt/install/lib')
+        self.cpp_info.libdirs = ["log4qt/lib"]
+        self.cpp_info.libs = tools.collect_libs(self, 'log4qt/lib')
 
         ## Extend the include path variable
-        self.cpp_info.includedirs.extend(['log4qt/install/include'])
+        self.cpp_info.includedirs.extend(['log4qt/include'])
